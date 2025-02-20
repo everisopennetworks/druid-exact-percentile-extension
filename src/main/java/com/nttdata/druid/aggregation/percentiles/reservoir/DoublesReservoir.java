d6125df0f7b44e25c7652b5fc32d68ae861c9251
David Reniz <drenizg@gmail.com>
2025-02-19 21:06:19 -0500

    Added `@JsonIgnore` annotation to `getStddev()` method

--- src/main/java/com/nttdata/druid/aggregation/percentiles/reservoir/DoublesReservoir.java
+++ src/main/java/com/nttdata/druid/aggregation/percentiles/reservoir/DoublesReservoir.java
@@ -19,6 +19,7 @@
 package com.nttdata.druid.aggregation.percentiles.reservoir;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.commons.collections.CollectionUtils;
@@ -117,6 +118,7 @@ public class DoublesReservoir implements Serializable {
         return percentiles;
     }
 
+    @JsonIgnore
     public double getStddev() {
         return new StandardDeviation().evaluate(reservoir.stream().mapToDouble(Double::doubleValue).toArray());
     }
